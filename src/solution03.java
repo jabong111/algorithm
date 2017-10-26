938c24211a9c38a201dbf397a68f542ba0e81c47
kimjihoon <kimjihoon@10.10.16.90>
2017-10-26 15:45:18 +0900

    integer to hexadecial

--- src/solution03.java
+++ src/solution03.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Test {	
+public class solution03 {	
 	public static void main(String args[]) {
 		Scanner scanner = new Scanner(System.in);
 		int Data = scanner.nextInt();
